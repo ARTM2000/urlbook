95abd96572ada2fef8bfc608bab15235afe82760
A.Tanoumandian <tanoomandian204@gmail.com>
2024-07-12 00:38:03 +0330

    fix: reconnecting loop in 5 seconds span added

--- internal/infra/config/mysql.go
+++ internal/infra/config/mysql.go
@@ -4,34 +4,42 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func NewMysqlDBConn(username, password, host, port, dbName string) *gorm.DB {
-	db, err := gorm.Open(
-		mysql.New(mysql.Config{
-			DSN: fmt.Sprintf(
-				"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-				username,
-				password,
-				host,
-				port,
-				dbName,
-			),
-		}),
-		&gorm.Config{},
-	)
+	var db *gorm.DB
+	var err error
 
-	if err != nil {
-		slog.LogAttrs(
-			context.Background(),
-			slog.LevelError,
-			"failed to connect to mysql database",
-			slog.Any("error", err.Error()),
+	for {
+		db, err = gorm.Open(
+			mysql.New(mysql.Config{
+				DSN: fmt.Sprintf(
+					"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+					username,
+					password,
+					host,
+					port,
+					dbName,
+				),
+			}),
+			&gorm.Config{},
 		)
-		panic(err)
+	
+		if err != nil {
+			slog.LogAttrs(
+				context.Background(),
+				slog.LevelError,
+				"failed to connect to mysql database",
+				slog.Any("error", err.Error()),
+			)
+			time.Sleep(time.Second * 5)
+			continue
+		}
+		break
 	}
 
 	return db
